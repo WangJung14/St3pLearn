f86a2fb67515d29ea9ba9e0a5402b241757f8938
Tommy <quangtrung54677@gmail.com>
2026-06-09 02:28:01 +0700

    feat:implement jwt token provider for token generation

---
+++ identity-service/src/main/java/com/tommy/identity/infrastructure/security/JwtTokenProvider.java
@@ -0,0 +1,76 @@
+package com.tommy.identity.infrastructure.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${jwt.refresh-expiration}")
+    private long refreshExpiration;
+
+    // Generate Access Token
+    public String generateAccessToken(UUID userId, String username) {
+        return buildToken(new HashMap<>(), userId, username, jwtExpiration);
+    }
+
+    // Generate Refresh Token
+    public String generateRefreshToken(UUID userId, String username) {
+        return buildToken(new HashMap<>(), userId, username, refreshExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UUID userId, String username, long expiration) {
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userId.toString()) // save userId into subject
+                .claim("username", username) // save username
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    // Get UserId from Token String
+    public UUID extractUserId(String token) {
+        String subject = extractAllClaims(token).getSubject();
+        return UUID.fromString(subject);
+    }
+
+    // Check token is valid ?
+    public boolean isTokenValid(String token) {
+        try {
+            return !extractAllClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ identity-service/src/main/java/com/tommy/identity/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.tommy.identity.infrastructure.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // config protect api
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // disable because use token
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // khong dung session
+                .authorizeHttpRequests(auth -> auth
+                        // Accept request for all api start with /api/auth/
+                        .requestMatchers("/api/auth/**").permitAll()
+                        // Another API (later) must have invalid token
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
