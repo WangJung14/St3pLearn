7fa312aba45acc0ad327041e8cdddb153aa45876
Tommy <quangtrung54677@gmail.com>
2026-06-08 12:43:12 +0700

    update: create root pom.xml

--- learning-service/src/test/java/com/tommy/learning/LearningApplicationTests.java
+++ learning-service/src/test/java/com/tommy/learning/LearningApplicationTests.java
@@ -3,7 +3,7 @@ package com.tommy.learning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration")
 class LearningApplicationTests {
     @Test
     void contextLoads() {
